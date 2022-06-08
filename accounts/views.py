8df5172b9a74db2b1f7708bb7a7dc74c33939af9
Silvio <silviotassini@gmail.com>
2022-06-08 14:05:41 -0300

    Updating products view

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 def home(request):
     return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products_list = Products.objects.all()
+    return render(request, 'accounts/products.html', {'products_list':products_list})
 
 def customer(request):
     return render(request, 'accounts/customer.html')
